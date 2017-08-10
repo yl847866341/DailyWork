5d95ce02d3516a86bb8d450cd5de574fba332d05
yl847866341 <847866341@qq.com>
2017-08-10 12:25:26 +0800

    Test Work

---
+++ Test/src/com/jxd/test/Test.java
@@ -0,0 +1,46 @@
+/**  
+ * 文件名：Test.java  
+ *  
+ * 版本信息：  
+ * 日期：2017年8月9日  
+ * Copyright Corporation 2017   
+ * 版权所有  
+ *  
+ */  
+    
+package com.jxd.test;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
+
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+
+/**  
+ * 
+ * @Title: Test.java     
+ * @Package com.jxd.test
+ * @author yuanlin
+ * @date 2017年8月9日 下午6:47:29  
+ */
+
+public class Test {
+	public static void main(String[] args) {
+//		String s = "袁林";
+//		try {
+//			String ns = new String(s.getBytes("gbk"),"utf-8");
+//			System.out.println(ns+"::::ss");
+//		} catch (UnsupportedEncodingException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println(s);
+//		try {
+//			String n2s = new String(s.getBytes("utf-8"),"gbk");
+//			System.out.println(n2s+"::::s2s");
+//		} catch (UnsupportedEncodingException e) {
+//			e.printStackTrace();
+//		}
+		
+		
+	}
+}
