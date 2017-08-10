2a806709b694958db05e8b3e12e15f5fd13c8541
yl847866341 <847866341@qq.com>
2017-08-10 16:48:50 +0800

    Handle conflicts.

--- Test/src/com/jxd/test/Test.java
+++ Test/src/com/jxd/test/Test.java
@@ -6,19 +6,18 @@
  * Copyright Corporation 2017   
  * 版权所有  
  *  
- */  
-    
+ */
+
 package com.jxd.test;
 
 import java.io.UnsupportedEncodingException;
 
-
-/**  
+/**
  * 
- * @Title: Test.java     
+ * @Title: Test.java
  * @Package com.jxd.test
  * @author yuanlin
- * @date 2017年8月9日 下午6:47:29  
+ * @date 2017年8月9日 下午6:47:29
  */
 
 public class Test {
@@ -26,7 +25,7 @@ public class Test {
 		String s = "yuanlin";
 		try {
 			String ns = new String(s.getBytes("gbk"),"utf-8");
-			System.out.println(ns+"::::ss");
+			System.out.println(ns+"-----ss");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -34,14 +33,15 @@ public class Test {
 		System.out.println(s);
 		try {
 			String n2s = new String(s.getBytes("utf-8"),"gbk");
-			System.out.println(n2s+"::::s2s");
+			System.out.println(n2s+"-----s2s");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		for(int i = 0;i < 10;i ++){
+		for (int i = 0; i < 10; i++) {
 			System.out.println("You are the right one！");
 		}
-		System.out.println("It is you who show me the world beauty.");
+		System.out.println("It is you who show me the world's beauty.");
+		System.out.println("Tomorrow");
 		System.out.println("Today ");
 	}
 }
